daa9af59f71bab3b9d485c047a478a2626e7f506
Jayshree Gupta <jayshreegupta1020@gmail.com>
2024-05-10 15:06:00 +0530

    Doctor and Hospital getter setter classes in dto are created.

--- src/main/java/cnu/dto/Doctor.java
+++ src/main/java/cnu/dto/Doctor.java
@@ -1,5 +1,24 @@
 package cnu.dto;
 
 public class Doctor {
-
+	protected int d_age;
+	protected String d_gender, d_specification;
+	public int getD_age() {
+		return d_age;
+	}
+	public void setD_age(int d_age) {
+		this.d_age = d_age;
+	}
+	public String getD_gender() {
+		return d_gender;
+	}
+	public void setD_gender(String d_gender) {
+		this.d_gender = d_gender;
+	}
+	public String getD_specification() {
+		return d_specification;
+	}
+	public void setD_specification(String d_specification) {
+		this.d_specification = d_specification;
+	}
 }
--- src/main/java/cnu/dto/Hospital.java
+++ src/main/java/cnu/dto/Hospital.java
@@ -1,5 +1,39 @@
 package cnu.dto;
 
-public class Hospital {
+import java.util.HashMap;
 
+public class Hospital {
+	protected int availBed;
+	protected String h_licenceno;
+	protected HashMap<String,Integer> availBlood = new HashMap<>();
+	public Hospital() {
+		this.availBed=0;
+		availBlood.put("A+",0);
+		availBlood.put("B+",0); 
+		availBlood.put("O+",0);
+		availBlood.put("A-",0);
+		availBlood.put("B-",0);
+		availBlood.put("O-",0);
+		availBlood.put("AB+",0);
+		availBlood.put("AB-",0);
+	}
+	public int getAvailBed() {
+		return availBed;
+	}
+	public void setAvailBed(int availBed) {
+		this.availBed = availBed;
+	}
+	public String getH_licenceno() {
+		return h_licenceno;
+	}
+	public void setH_licenceno(String h_licenceno) {
+		this.h_licenceno = h_licenceno;
+	}
+	public HashMap<String, Integer> getAvailBlood() {
+		return availBlood;
+	}
+	public void setAvailBlood(String type, int unit) {
+		this.availBlood.put(type, unit);
+	}
+	
 }
