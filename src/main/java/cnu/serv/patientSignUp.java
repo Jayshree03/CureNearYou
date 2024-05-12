b5014cb5f601b33846786d91a41d8118d5ceaad5
Jayshree Gupta <jayshreegupta1020@gmail.com>
2024-05-12 15:37:51 +0530

    Some servlets are added

--- src/main/java/cnu/serv/patientSignUp.java
+++ src/main/java/cnu/serv/patientSignUp.java
@@ -7,19 +7,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import cnu.dto.Doctordto;
+import cnu.dto.Patientdto;
 import cnu.dto.Userdto;
 
 /**
- * Servlet implementation class DoctorSignUp
+ * Servlet implementation class patientSignUp
  */
-public class DoctorSignUp extends HttpServlet {
+public class patientSignUp extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public DoctorSignUp() {
+    public patientSignUp() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,7 +30,7 @@ public class DoctorSignUp extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		Userdto udto = new Userdto();
-		Doctordto ddto = new Doctordto(); 
+		Patientdto ddto = new Patientdto();
 	}
 
 	/**
